547ba3ef5b33313b31c7cbb1210732b41252b216
Rennan-Ferreira <rm99364@fiap.com.br>
2023-11-16 10:58:40 -0300

    home criada 1440x1024

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -41,7 +41,7 @@ export default function Home() {
         
       <div className={styles.sectionInfos}> 
         <div className={styles.imgInfo}>
-          <Image src='/img/rosa-lupus-1.png' alt='rosácea e lúpus' width={400} height={470}/>
+          <Image src='/img/rosa-lupus-1.png' alt='rosácea e lúpus' width={430} height={470}/>
         </div>
         <div className={styles.textInfo}>
           <h2>Mais Informação, Mais Saúde</h2>
@@ -49,6 +49,42 @@ export default function Home() {
           <button>Saiba mais</button>
         </div>
       </div>
+      <div className={styles.sectionInspiracao}>
+        <h2>Nossa Inspiração</h2>
+        <div className={styles.sublinhados2}>
+          <div className={styles.sublinhado2}></div>
+          <div className={styles.sublinhado2}></div>
+        </div>
+        <p>Saiba mais sobre a nossa motivação e a nossa história por trás do aplicativo para rosácea e lúpus.</p>
+        <button>Saiba mais</button>
+      </div>
+      <div className={styles.sectionAvaliacoes}>
+        <h2>Avaliações dos nossos usuários</h2>
+        <div className={styles.blocoAvaliacao}>
+          <div className={styles.conteudoAvaliacoes}>
+            <p>Eu descobri que tenho lúpus pelo app, que me mostrou os sintomas e como marcar a consulta. O médico confirmou o diagnóstico e me explicou o tratamento. O app me ajudou a monitorar os sintomas e a me informar sobre o lúpus. O app foi essencial para mim, pois me fez conhecer a minha doença e me orientou sobre como cuidar dela. Parabéns aos desenvolvedores pelo ótimo app.</p>
+          </div>
+          <div className={styles.perfilAvaliador}>
+            <Image className={styles.imgAvaliacao} src='/img/img-avaliador.png' alt='mulher' width={70} height={70}/>
+            <div className={styles.infoAvaliador}>
+              <h6>Márcia Castro</h6>
+              <p>Brasil</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className={styles.footer}>
+        <div className={styles.conteudoFooter}>
+          <Image  className={styles.imgFooter} src='/img/logo-alliance.png' alt='logo' width={100} height={100}/>
+          <div className={styles.sublinhadoFooter}></div>
+          <h4>Integrantes:</h4>
+          <p>Harthley Fernandes Teixeira Gomes - RM551359 - 1TDSPF</p>
+          <p>Isabella Jorge Ferreira - RM552329 - 1TDSPF</p>
+          <p>Jaisy Cibele Alves - RM552269 - 1TDSPF</p>
+          <p>Rennan Ferreira da Cruz - RM99364 - 1TDSPF</p>
+          <p>Thiago de Jesus Barbosa - RM552260 - 1TDSPF</p>
+        </div>
+      </footer>
     </main>
   )
 }
