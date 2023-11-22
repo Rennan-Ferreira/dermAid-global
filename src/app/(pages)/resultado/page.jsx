a59123eeb73d14f658ffda1791d1b1f0e4b570b4
Rennan-Ferreira <rm99364@fiap.com.br>
2023-11-22 09:47:12 -0300

    página 'resultado' parcialmente terminada

---
+++ src/app/(pages)/resultado/page.jsx
@@ -0,0 +1,21 @@
+import styles from '../../../styles/resultado.module.css'
+
+export default function Resultado() {
+  return (
+    <main>
+        <div className={styles.topSection}>
+            <h1>80% de chance de ser Lúpus</h1>
+            <h5>O tratamento inclui:</h5>
+            <p>* Uso de medicamentos que modulam o sistema imunológico e controlam a inflamação, como anti-inflamatórios, corticoides, antimaláricos, imunossupressores e drogas biológicas</p>
+            <p>* Cuidados gerais com a saúde, como usar protetor solar, manter uma alimentação equilibrada, fazer repouso adequado e evitar situações de estresse.</p>
+            <p>* Seguimento médico regular e exames periódicos para monitorar a evolução da doença e os efeitos dos medicamentos</p>
+            <p>* Apoio psicológico e social para lidar com os aspectos emocionais e as limitações impostas pela doença.</p>
+        </div>
+        <div className={styles.bottomSection}>
+            <p>Recomendamos que você consulte um dermatologista e inicie o tratamento o mais breve possivel.</p>
+            <button className={styles.btnAgendar}>Agendar Consulta</button>
+            <button className={styles.btnVoltar}>Voltar para a tela inicial</button>
+        </div>
+    </main>
+  )
+}
